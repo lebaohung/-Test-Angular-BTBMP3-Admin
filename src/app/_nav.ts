c63d886f7ff640f375d48e7a214adccd67c6b2cb
lebaohung.1996 <lebaohung.1996@gmail.com>
2020-07-24 14:45:25 +0700

    hung sort fix form admin homepage

--- src/app/_nav.ts
+++ src/app/_nav.ts
@@ -11,24 +11,18 @@ export const navItems: INavData[] = [
     }
   },
   {
+    name: 'User List',
+    url: '',
+    icon: 'icon-speedometer',
+    badge: {
+      variant: 'info',
+      text: 'NEW'
+    }
+  },
+  {
     name: 'Disabled',
     url: '/dashboard',
     icon: 'icon-ban',
     attributes: { disabled: true },
-  },
-  {
-    name: 'Download CoreUI',
-    url: 'http://coreui.io/angular/',
-    icon: 'icon-cloud-download',
-    class: 'mt-auto',
-    variant: 'success',
-    attributes: { target: '_blank', rel: 'noopener' }
-  },
-  {
-    name: 'Try CoreUI PRO',
-    url: 'http://coreui.io/pro/angular/',
-    icon: 'icon-layers',
-    variant: 'danger',
-    attributes: { target: '_blank', rel: 'noopener' }
   }
 ];
